807f08a8f785fb4fbac18004a2dd11a84aa5ccb3
KaanBobac <bobackaan@gmail.com>
2022-09-20 09:38:22 +0200

    refactor - remove global variables

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-gameTable = [' ']*10
-possibleMoves = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-player_no = 1
 
-def play():
-    next_move = get_move()
-    move(next_move[0], next_move[1])
+def play(player_no,game_table):
+    next_move = get_move(player_no,game_table)
+    return move(game_table, next_move)
 
 
-def get_move():
-    position = get_position()
-    item = get_item()
+def get_move(player_no,game_table):
+    position = get_position(player_no,game_table)
+    item = get_item(player_no)
     return position, item
 
 
-def get_item():
+def get_item(player_no):
     while True:
         item = input('Player{} please enter your next item'.format(player_no))
         if is_proper_item(item):
@@ -23,22 +20,22 @@ def get_item():
     return item
 
 
-def get_position():
+def get_position(player_no,game_table):
     while True:
         try:
             position = int(input('Player{} please enter your next item position'.format(player_no)))
-            if is_proper_position(position):
+            if is_proper_position(position, game_table):
                 break
             else:
-                display_possible_moves()
+                display_possible_positions()
         except ValueError:
-            display_possible_moves()
+            display_possible_positions()
     return position
 
 
-def is_proper_position(position):
-    for i in possibleMoves:
-        if i == position:
+def is_proper_position(position, game_table):
+    for i in range(1, 10):
+        if i == position and game_table[i - 1] == ' ':
             return True
     return False
 
@@ -50,9 +47,11 @@ def is_proper_item(item):
         return False
 
 
-def move(position, item):
-    gameTable[position - 1] = item
-    possibleMoves[position - 1] = ''
+def move(game_table, next_move):
+    position = next_move[0]
+    item = next_move[1]
+    game_table[position - 1] = item
+    return game_table
 
 
 def display(table):
@@ -64,68 +63,74 @@ def display(table):
             row = ''
 
 
-def is_finished():
-    return is_board_full() or is_column_match() or is_row_match() or is_cross_match()
+def is_finished(game_table):
+    return is_board_full(game_table) or is_column_match(game_table) or is_row_match(game_table) or is_cross_match(
+        game_table)
 
 
-def is_board_full():
-    for move in possibleMoves:
-        if move != '':
+def is_board_full(game_table):
+    for place in game_table:
+        if place == ' ':
             return False
     return True
-def is_column_match():
+
+
+def is_column_match(game_table):
     for i in range(0, 3):
-        if gameTable[i] == gameTable[i + 3] and gameTable[i + 3] == gameTable[i + 6] and gameTable[i + 6] != ' ':
+        if game_table[i] == game_table[i + 3] and game_table[i + 3] == game_table[i + 6] and game_table[i + 6] != ' ':
             return True
     return False
 
 
-def is_row_match():
+def is_row_match(game_table):
     row_number = [0, 3, 6]
     for i in row_number:
-        if gameTable[i] == gameTable[i + 1] and gameTable[i + 1] == gameTable[i + 2] and gameTable[i + 2] != ' ':
+        if game_table[i] == game_table[i + 1] and game_table[i + 1] == game_table[i + 2] and game_table[i + 2] != ' ':
             return True
     return False
 
 
-def is_cross_match():
-    if gameTable[0] == gameTable[4] and gameTable[4] == gameTable[8] and gameTable[8] != ' ':
+def is_cross_match(game_table):
+    if game_table[0] == game_table[4] and game_table[4] == game_table[8] and game_table[8] != ' ':
         return True
-    if gameTable[2] == gameTable[4] and gameTable[4] == gameTable[6] and gameTable[6] != ' ':
+    if game_table[2] == game_table[4] and game_table[4] == game_table[6] and game_table[6] != ' ':
         return True
     else:
         return False
 
 
 def start():
+    game_table = [' '] * 10
+    player_no = 1
     display_welcome_message()
-    while not is_finished():
-        display(gameTable)
-        play()
-        change_player()
+    display_possible_items()
+    display_possible_positions()
+    while not is_finished(game_table):
+        display(game_table)
+        game_table = play(player_no,game_table)
+        player_no = change_player(player_no)
     print('Game is Over!')
-    print('---This is final result----')
-    display(gameTable)
+    print('---This is the final result----')
+    display(game_table)
 
 
 def display_welcome_message():
     print('***************************')
     print('Welcome to TIC TAC TOE Game')
     print('***************************')
-    display_possible_moves()
-
-
-def display_possible_moves():
-    display_possible_items()
-    display(possibleMoves)
-    print('***************************')
 
 
 def display_possible_items():
-    print('Please make your X or O moves by the following position numbers')
+    print('Please make your X or O move')
+    print('***************************')
 
+def display_possible_positions():
+    possible_moves = range(1, 10)
+    print('Please select your next move position numbers')
+    display(possible_moves)
+    print('***************************')
 
-def change_player():
+def change_player(player_no):
     if player_no == 2:
         return 1
     else:
